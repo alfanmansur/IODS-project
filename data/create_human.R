15b883b47943eef9f7986da1ed1f92b594ae8e4a
alfanmansur <alfan.mansur@helsinki.fi>
2021-12-01 14:56:05 +0200

    01.12.2021 draft all

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,7 @@
 # Alfan Mansur
-# 25.11.2021
+# Firstly created in 25.11.2021
+# Continued on 29.11.2021 starting from line 40 for Exercise 5
+# The 'human' dataset is about Human Development Index which originates from the United Nations Development Programme.
 
 # Read the “Human development” and “Gender inequality” datas into R
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -35,4 +37,34 @@ dim(human)   #check the dimension: 195 observations and 19 variables
 
 write.csv(human,"C:/Users/Lenovo/Documents/R/IODS-project/data/human.csv", row.names = FALSE)
 
+#29.11.2021: Continued for Exercise 5
+# Load the data from my local folder
+myData <- read.csv('./data/human.csv')
+# Load the data from the provided link
+human <- read.table(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt"),sep=",",header = TRUE)
+# Both data sets are the same except the names of some variables. For the sake of simplicity, I will use the dataset from the provided link.
+
+# Transform 'GNI' to numeric
+str(human$GNI)  # look at the structure of GNI variable
+human$GNI <- as.numeric(gsub(",", ".", human$GNI))
+str(human$GNI)  # look at the structure of GNI variable again, it is now numeric
+
+# Exclude unneeded variables
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+
+# Remove all rows with missing values 
+human <- filter(human, complete.cases(human))
+
+# Remove the observations which relate to regions instead of countries
+tail(human, 10) #look at the last 10 observations
+last <- nrow(human) - 7   #last indice we want to keep
+human <- human[1:last, ]
+
+rownames(human) <- human$Country #Define the row names of the data by the country names
+human <- human[-1]  #remove the country name column from the data
+dim(human)          #check the dimension: 155 x 8
+
+# Save the human data in data folder including the row names
+write.csv(human,"C:/Users/Lenovo/Documents/R/IODS-project/data/human.csv", row.names = TRUE)
 
